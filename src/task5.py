da16cb9ae6dc77495b9424064bbdba1d5b01f4eb
COM2009 Student <student@TEND89EF39FF329.shefuniad.shef.ac.uk>
2021-05-21 15:18:19 +0100

    commit maze navigation part of task5

--- src/task5.py
+++ src/task5.py
@@ -53,6 +53,8 @@ class Task5(object):
         self.direct_left = 1.0
         self.direct_right = 1.0
 
+        self.small_distance_front = 1.0
+
         self.robot_controller = MoveTB3()
         self.robot_odom = TB3Odometry()
         self.turn_vel_fast = -0.5
@@ -66,7 +68,7 @@ class Task5(object):
         self.exploring = False
         self.navigating_maze = False
         self.targetting = False
-        
+
         self.rate = rospy.Rate(5)
 
         self.m00 = 0
@@ -146,12 +148,36 @@ class Task5(object):
         self.direct_right = scan_data.ranges[-90]
         self.direct_front = scan_data.ranges[0]
 
+        # get narrower scan data, used in maze navigation
+        narrow_left = scan_data.ranges[0:16]
+        narrow_right = scan_data.ranges[-15:]
+        narrow_front = np.array(narrow_left + narrow_right)
+        self.narrow_distance_front = narrow_front.min()
+
+        small_left = scan_data.ranges[16:31]
+        self.small_distance_left = np.array(small_left).min()
+
+        small_right = scan_data.ranges[290:342]
+        self.small_distance_right = np.array(small_right).min()
+
+        left_arc = scan_data.ranges[0:10]
+        right_arc = scan_data.ranges[-10:]
+        front_arc2 = np.array(left_arc + right_arc)
+        self.thin_front = front_arc2.min()
+
+        # get initial robot odometry
+        self.posx0 = self.robot_odom.posx
+        self.posy0 = self.robot_odom.posy
+
+        # check if the robot has moved far enough from the start point to detect the beacon
+        self.in_range = False
 
     # method to change linear/angular velocities and publish to controller
     def change_vels(self, linear, angular):
         self.robot_controller.set_move_cmd(linear, angular)
         self.robot_controller.publish()
 
+    # method to turn the robot given the direction
     def turn(self, direction):
         self.robot_controller.stop()
         # rotate robot by 90deg in given direction
@@ -159,38 +185,14 @@ class Task5(object):
             self.change_vels(0, 1.0)
         elif direction == "right":
             self.change_vels(0, -1.0)
+        elif direction == "backwards":
+            self.change_vels(0, 1)
+            rospy.sleep(pi/2)
 
         # wait for the robot to do a 90deg turn
         # minus 0.055 seconds because ROS overturns
         rospy.sleep((pi/2)-0.055)
         self.robot_controller.stop()
-
-    # method to move the robot forward and re-adjust
-    def recentre(self, d_left, d_right):
-        print("re-centering robot")
-        
-        # set robots target angle 
-        curr_angle = self.robot_odom.yaw
-        target_angle = 0
-        if abs(curr_angle) < 45:
-            target_angle = 0
-        elif curr_angle > 45 and curr_angle < 135:
-            target_angle = 90
-        elif abs(curr_angle) > 135:
-            target_angle = 180
-        elif curr_angle < -45 and curr_angle > -135:
-            target_angle = -90
-
-        angular_vel = radians(target_angle - curr_angle)
-
-        # re-centre robot
-        if d_right < 0.25:
-            angular_vel += 0.4
-        elif d_left < 0.25:
-            angular_vel -= 0.4
-
-        # adjust angular velocity to re-centre
-        self.change_vels(0.25, angular_vel)
 
     # method to find the target colour - used at the start
     def get_target_colour(self):
@@ -212,68 +214,40 @@ class Task5(object):
                     self.target_colour))
 
         self.robot_controller.set_move_cmd(0.0, -0.6)
-        tick2 = 18
+        tick2 = 17
         for i in range(tick2):
             self.robot_controller.publish()
             self.rate.sleep()
         self.robot_controller.set_move_cmd(0.0, 0.0)
 
+    # check the odometry 
+    def check_odom(self):
+        self.distance_from_start = sqrt(pow(
+            self.posx0 - self.robot_odom.posx, 2) + pow(self.posy0 - self.robot_odom.posy, 2))
+
+        # return robot displacement
+        return self.distance_from_start
+        
     # method for robot to navigate through the maze
     def navigate_maze(self, d_front, d_left, d_right):
-        # turning truth table
-        # notF = move forwards
-        # F, R = turn left
-        # F, L = turn right
-        # F, notR, notL = three-way turn
-    
-        # set direction variables
-        d_front = self.distance_front
-        d_right = self.distance_right
-        d_left = self.distance_left
-
-        # if front wall not too close, move forward
-        if d_front > 0.5:
-            print("robot moving forward")
-            self.recentre(d_left, d_right)
-
-        elif d_right < 0.4:
-            self.change_vels(0.2, 0.3)
-        elif d_right > 0.5:
-            self.change_vels(0.2, -0.3)
-
-        elif d_front < 0.5 and d_left > 0.5 and d_right > 0.5:
-            self.turn("right")
-
-        # if front and left walls too close, turn right
-        elif d_front < 0.6 and d_left < 0.55:
-            print("robot turning right")
-            self.turn("right")
-
-        # if front and right walls too close turn left
-        elif (d_front < 0.6 and d_right < 0.55):
-            print("robot turning left")
-            self.turn("left")
-
-        # check for a three-way turn and turn accordingly
-        # elif (d_front < 0.6 and d_right > 0.55 and d_left > 0.55):
-        #     print("at a three-way turn")
-        #     if self.threeway_turn_num == 0:
-        #         print("robot turning left")
-        #         self.turn("left")
-        #         self.threeway_turn_num += 1
-        #     elif self.threeway_turn_num == 1:
-        #         print("robot turning right")
-        #         self.turn("right")
-        #         self.threeway_turn_num += 1
-        #     elif self.threeway_turn_num == 2:
-        #         print("robot turning left")
-        #         self.turn("left")
-        #     else:
-        #         pass
+        # true if robot is at a dead end and can only turn around
+        if d_front < 0.2 and d_left < 0.2 and d_right < 0.2:
+            print("turning backwards")
+            self.turn("backwards")
+        # true if there is space in front and to the right
+        elif d_front > 0.325 and d_right > 0.325:
+            self.change_vels(0.2, -0.81)
+        # true if there is space in front but not to the right
+        elif d_front > 0.325 and d_right < 0.325:
+            self.change_vels(0.12, 0.5)
+        # true if there is not space in front but is to the right
+        elif d_front < 0.325 and d_right > 0.325:
+            self.change_vels(0.0, -0.51)
+        # true if there is not space in front or to the right
+        elif d_front < 0.325 and d_right < 0.325:
+            self.change_vels(0.0, 0.52)
         else:
-            pass
-        
-        # self.navigating_maze = False
+            self.change_vels(0, -0.5)
 
     # method for the robot to explore the arena - used after robot passes the maze
     def explore(self, d_goal, d_front, d_left, d_right):
@@ -321,7 +295,7 @@ class Task5(object):
             self.change_vels(0.2, 0)
 
         # true if there is a lot of space to the left, but not in front or the right
-        elif self.move_rate == 'fast'  and d_front < d_goal and d_left > d_goal and d_right < d_goal:
+        elif self.move_rate == 'fast' and d_front < d_goal and d_left > d_goal and d_right < d_goal:
             self.robot_controller.stop()
             self.change_vels(0.1, 0.8)
 
@@ -330,13 +304,13 @@ class Task5(object):
             self.robot_controller.stop()
             self.change_vels(0.1, -0.8)
 
-        elif self.move_rate == 'slow':
+        elif self.move_rate == 'slow' and self.check_odom > 1.5:
             print("BEACON DETECTED: Beaconing initiated.")
             self.robot_controller.stop()
             self.change_vels(0.3, 0.0)
             self.targetting = True
 
-        elif self.targetting and (d_front < 0.5 or d_left < 0.2 or d_right < 0.2):
+        elif self.targetting and (d_front < 0.5 or d_left < 0.2 or d_right < 0.2) and self.check_odom > 1.5:
             print("BEACONING COMPLETE: The robot has now stopped.")
             self.robot_controller.stop()
             self.change_vels(0.0, 0.0)
@@ -347,24 +321,25 @@ class Task5(object):
     # system main loop
     def main_loop(self):
         r = rospy.Rate(100)
-        
+
         # get the target colour from the start zone if not found
         if self.target_colour == "none":
             self.get_target_colour()
-            # move the robot out of the start zone then start navigating the maze
+            # move the robot out of the start zone and start navigating
             print("moving out of the start zone")
             while self.direct_left < 0.5 and self.direct_right < 0.5:
                 self.change_vels(0.2, 0)
-                rospy.sleep(1)
+                rospy.sleep(0.8)
             print("robot has left the start zone")
             self.robot_controller.stop()
             self.navigating_maze = True
 
         # naviagate the maze while self.navigating_maze is true
         while self.navigating_maze:
-            self.navigate_maze(self.direct_front, self.direct_left, self.direct_right)
+            self.navigate_maze(self.narrow_distance_front,
+                               self.small_distance_left, self.small_distance_right)
 
-        # set the robot to exploring the arena while self.exploring is true - after maze is passed
+        # set the robot to exploring the arena while self.exploring is true and in range
         while self.exploring:
             if self.m00 > self.m00_min:
                 # blob detected
@@ -376,13 +351,14 @@ class Task5(object):
             else:
                 self.move_rate = 'fast'
 
-            self.explore(0.6, self.distance_front, self.distance_left, self.distance_right)
-
+            self.explore(0.6, self.distance_front,
+                         self.distance_left, self.distance_right)
 
     # shutdownhook to allow ctrl+c to stop the program
     def shutdownhook(self):
         self.shutdown_function()
         self.ctrl_c = True
+
 
 if __name__ == '__main__':
     task5 = Task5()
